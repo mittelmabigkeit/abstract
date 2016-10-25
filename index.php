3b236d8fc53a4050e062903fee696b402d0e5f90
mittelmabigkeit <comp422012016@mail.ru>
2016-10-25 15:35:02 +0300

    abstract

---
+++ index.php
@@ -0,0 +1,13 @@
+<meta charset="utf-8">
+<?php
+/**
+ * Created by PhpStorm.
+ * User: html
+ * Date: 25.10.2016
+ * Time: 15:32
+ */
+
+require_once 'Pbase.php';
+
+$obj = new Pbase();
+$obj -> test();
